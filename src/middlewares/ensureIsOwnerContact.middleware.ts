03f0baf138a9a4bfba08153a34fc2fb18cab5923
Jessica Estevam <jessicafpereira16@gmail.com>
2023-07-26 17:53:04 -0300

    feat:applied contact owner verification

--- src/middlewares/ensureIsOwnerContact.middleware.ts
+++ src/middlewares/ensureIsOwnerContact.middleware.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Contact } from "../entities/contact.entitie";
+import { AppError } from "../errors/appError";
 
 const ensureIsOwnerContactMiddleware = async (
   req: Request,
@@ -6,7 +9,22 @@ const ensureIsOwnerContactMiddleware = async (
   next: NextFunction
 ): Promise<Response | void> => {
   const userId = res.locals.id;
-  const idContact = req.params.id;
+  const idContact: number = parseInt(req.params.id);
+
+  const contactRepository = AppDataSource.getRepository(Contact);
+
+  const contact = await contactRepository.findOne({
+    where: { id: idContact },
+    relations: { user: true },
+  });
+  if (!contact) {
+    return res.status(404).json({ message: "Contact not found" });
+  }
+  if (contact.user.id != userId) {
+    return res.status(404).json({ message: "You don't have permissions" });
+  }
 
   return next();
 };
+
+export { ensureIsOwnerContactMiddleware };
--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -12,6 +12,7 @@ import {
   contactSchemaRequest,
 } from "../schemas/contact.schemas";
 import { verifyTokenMiddleware } from "../middlewares/verifyToken.middleware";
+import { ensureIsOwnerContactMiddleware } from "../middlewares/ensureIsOwnerContact.middleware";
 
 const contactsRoutes = Router();
 
@@ -21,14 +22,30 @@ contactsRoutes.post(
   verifyTokenMiddleware,
   createContactController
 );
-contactsRoutes.get("", verifyTokenMiddleware, listContactController);
-contactsRoutes.get("/:id", verifyTokenMiddleware, readContactController);
+contactsRoutes.get(
+  "",
+  verifyTokenMiddleware,
+  ensureIsOwnerContactMiddleware,
+  listContactController
+);
+contactsRoutes.get(
+  "/:id",
+  verifyTokenMiddleware,
+  ensureIsOwnerContactMiddleware,
+  readContactController
+);
 contactsRoutes.patch(
   "/:id",
   validatedDataMiddleware(contactSchemaPartial),
   verifyTokenMiddleware,
+  ensureIsOwnerContactMiddleware,
   updateContactController
 );
-contactsRoutes.delete("/:id", verifyTokenMiddleware, deleteContactController);
+contactsRoutes.delete(
+  "/:id",
+  verifyTokenMiddleware,
+  ensureIsOwnerContactMiddleware,
+  deleteContactController
+);
 
 export { contactsRoutes };
